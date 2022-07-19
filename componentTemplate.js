0b644d0015b81b8820802ece2cef4e1be66b74e3
OwenMG <36777755+OwenMG@users.noreply.github.com>
2022-07-18 22:13:21 -0700

    creates component skeletons

---
+++ componentTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/.css';
+
+const styles = {
+
+};
+
+function () {
+    return (
+
+    );
+}
+
+export default ;
---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Section.css';
+
+const styles = {
+    sectionStyle: {
+
+    }
+};
+
+function Section() {
+    return (
+        <section style={styles.sectionStyle} className="section">
+            
+        </section>
+    );
+}
+
+export default Section;
